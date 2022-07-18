7f4cad5dc49935e0c471e158d616479147a844de
Mosiur Rahman <mosiur.404.rahman@gmail.com>
2022-07-18 18:41:33 +0600

    updated srtf

--- os/srtf.cpp
+++ os/srtf.cpp
@@ -26,8 +26,8 @@ private:
   queue<Process> processQueue;
   priority_queue<Process, vector<Process>, Compare> readyQueue;
   queue<Process> terminatedQueue;
-  vector<int> timestamps;
-  vector<int> history;
+  vector<int> timestamps; // extra
+  vector<int> history;    // extra
   unsigned int totalCT, totalTAT, totalWT, totalIdle, counter;
   unsigned int duration = 0;
 
@@ -38,54 +38,51 @@ public:
   }
 
   void init() {
-    int prevCT = 0;
-    this->duration = 0;
     while (!processQueue.empty() || !this->readyQueue.empty()) {
-      this->checkProcessQueue();
+      this->checkProcessQueue(); // fill the readyQueue initially
+      this->handleIdle();
       while (!this->readyQueue.empty()) {
         Process prc = readyQueue.top();
         readyQueue.pop();
 
-        // calc start
-        if (prc.startTime == -1) {
-          prc.startTime = this->duration;
-          prc.waitingTime += prc.startTime - prc.arrivalTime;
+        /*  calc start */
+
+        if (prc.startTime == -1) {        // if process is new
+          prc.startTime = this->duration; // set start time
+          prc.waitingTime += prc.startTime - prc.arrivalTime; // waiting time
         }
-        prc.lastST = this->duration; // last start counter
+        prc.lastST = this->duration; // last start counter(for waiting)
 
         // after the inital start of process
         if (prc.remainingBurstTime != prc.burstTime)
-          prc.waitingTime += prc.lastST - prc.completionTime;
-        // calc end
+          prc.waitingTime += prc.lastST - prc.completionTime; // calc waiting
 
-        // loop until we find a process with less time
+        /* calc end */
+
+        // loop until we find a process with less remaining time
         while (prc.remainingBurstTime > 0) {
           if (!readyQueue.empty() &&
               prc.remainingBurstTime > readyQueue.top().remainingBurstTime)
             break;
 
-          prc.remainingBurstTime--; // decrease burst time
-          this->duration++;         // increase clock time
-          this->checkProcessQueue();
+          prc.remainingBurstTime--;  // decrease burst time
+          this->duration++;          // increase clock time
+          this->checkProcessQueue(); // check and update ready queue
         }
         prc.completionTime = this->duration; // update completion time
 
         if (prc.remainingBurstTime <= 0) {
-          counter++;
+          this->counter++;
           this->terminatedQueue.push(prc);
         } else
           this->checkProcessQueue()->readyQueue.push(prc);
 
         // extra
-        this->setTimestamp(prc);
+        this->setTimestamp(prc); // gantt chart
 
         this->totalCT += prc.completionTime;
         this->totalTAT += prc.turnArroundTime();
         this->totalWT += prc.waitingTime;
-        // this->totalIdle += prevCompletionTime < prc.arrivalTime
-        //                        ? prc.arrivalTime - prevCompletionTime
-        //                        : 0;
-        // prevCompletionTime = prc.completionTime;
       }
     }
   }
@@ -99,6 +96,15 @@ public:
     }
     return this;
   }
+
+  Scheduler *handleIdle() {
+    if (readyQueue.empty()) {
+      int dt = processQueue.front().arrivalTime - this->duration;
+      this->totalIdle += dt;
+      this->duration += dt;
+    }
+    return this;
+  }
   // extra
   void setTimestamp(Process &prc) {
     this->history.push_back(prc.index);
@@ -108,12 +114,13 @@ public:
   int min(int a, int b) { return a < b ? a : b; }
 
   void print() {
+    cout << "Gantt Chart: " << endl;
     for (int index : this->history)
       cout << "|  " << 'P' << index << "   ";
     cout << '|' << endl;
     for (int timestamp : this->timestamps)
       cout << timestamp << '\t';
-    cout << endl;
+    cout << endl << endl;
 
     cout << "ID\tArrival\tBurst\tStart\tCompletion\tTAT\tWaiting" << endl;
     while (!this->terminatedQueue.empty()) {
@@ -139,8 +146,9 @@ public:
   }
 
   Scheduler() {
-    this->totalCT = 0, this->totalTAT = 0, this->totalWT = 0, this->counter = 0,
-    this->totalIdle = 0;
+    this->totalCT = this->totalTAT = this->totalWT = this->counter =
+        this->totalIdle = this->duration = 0;
+
     timestamps.push_back(0); // intial 0
   }
   ~Scheduler() {}
@@ -150,7 +158,7 @@ int main(int argc, char const *argv[]) {
   Scheduler *srtf = new Scheduler();
   srtf->enqueue({1, 0, 7})->enqueue({2, 2, 4});
   srtf->enqueue({5, 3, 5})->enqueue({3, 4, 1});
-  srtf->enqueue({4, 5, 4});
+  srtf->enqueue({4, 5, 4})->enqueue({6, 25, 2});
   srtf->init();
   srtf->print();
   return 0;
